17bdfcd67462e86aa0de48e0532a495ef6b3e144
Zihao <zihao-lee@outlook.com>
2023-02-14 18:05:09 +0800

    Rapid API Music Fetching

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '312b645563msh6c7bc63e6fd5c66p16d943jsn613d8201e128');
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
