9d95a69006311568bd8ce971080a1eec785ca3fa
Zihao <zihao-lee@outlook.com>
2023-02-14 21:02:39 +0800

    Loader&Error&SongCard

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,12 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = 'Pop';
 
@@ -22,7 +26,7 @@ const Discover = () => {
           className="bg-black text-gray-300 p-3 text-sm
                                   rounded-lg outline-none sm:mt-0 mt-5"
         >
-          {data?.map((genre) => (
+          {genres?.map((genre) => (
             <option key={genre.value} value={genre.value}>
               {genre.title}
             </option>
@@ -31,10 +35,13 @@ const Discover = () => {
       </div>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data?.map((song, i) => (
           <SongCard
             key={song.key}
             song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
             i={i}
           />
         ))}
